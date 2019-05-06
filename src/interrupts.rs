7c125ada69d1d6993161170c59ca9093fca80e3b
gky360 <gky360@gmail.com>
2019-05-07 01:58:50 +0900

    [post-06] Double Faults

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,3 +1,4 @@
+use crate::gdt;
 use crate::println;
 use lazy_static::lazy_static;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
@@ -9,6 +10,12 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(break_point_handler);
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(double_fault_handler)
+                .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+
         idt
     };
 }
@@ -19,6 +26,13 @@ pub fn init_idt() {
 
 extern "x86-interrupt" fn break_point_handler(stack_frame: &mut InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: &mut InterruptStackFrame,
+    _error_code: u64,
+) {
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
 
 #[test_case]
--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,12 @@ pub extern "C" fn _start() -> ! {
 
     rustos::init();
 
-    // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3();
+    fn stack_overflow() {
+        stack_overflow(); // for each recursion, the return address is pushed
+    }
+
+    // uncomment line below to trigger a stack overflow
+    // stack_overflow();
 
     #[cfg(test)]
     test_main();
