46d1c05cd70540ac25e1ef016fc70dbc89956a24
gky360 <gky360@gmail.com>
2019-12-07 03:24:52 +0900

    [chap-03] remove x86_64 crate from dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 #![no_std]
 #![cfg_attr(test, no_main)]
+#![feature(asm)]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use x86_64::instructions::hlt;
 
 pub mod serial;
 pub mod vga_buffer;
@@ -25,6 +25,27 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     loop_hlt()
 }
 
+use core::marker::PhantomData;
+
+struct Port {
+    port: u16,
+    phantom: PhantomData<u32>,
+}
+
+impl Port {
+    pub const fn new(port: u16) -> Port {
+        Port {
+            port,
+            phantom: PhantomData,
+        }
+    }
+
+    #[inline]
+    pub unsafe fn write(&mut self, value: u32) {
+        asm!("outl %eax, %dx" :: "{dx}"(self.port), "{eax}"(value) :: "volatile");
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u32)]
 pub enum QemuExitCode {
@@ -33,8 +54,6 @@ pub enum QemuExitCode {
 }
 
 pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
     unsafe {
         let mut port = Port::new(0xf4);
         port.write(exit_code as u32);
@@ -58,5 +77,11 @@ fn panic(info: &PanicInfo) -> ! {
 pub fn loop_hlt() -> ! {
     loop {
         hlt();
+    }
+}
+
+fn hlt() {
+    unsafe {
+        asm!("hlt" :::: "volatile");
     }
 }
