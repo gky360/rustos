598a823e680dc2c040fdd56278e0002eb9edb07d
gky360 <gky360@gmail.com>
2019-12-08 04:16:07 +0900

    [chap-04] add tests for vga

--- src/vga.rs
+++ src/vga.rs
@@ -6,11 +6,11 @@ lazy_static! {
         Color(0x00, 0x00, 0x00), // 0 : Black
         Color(0xff, 0x00, 0x00), // 1 : Red
         Color(0x00, 0xff, 0x00), // 2 : Green
-        Color(0xff, 0xff, 0x00), // 3 :Yellow
+        Color(0xff, 0xff, 0x00), // 3 : Yellow
         Color(0x00, 0x00, 0xff), // 4 : Blue
-        Color(0xff, 0x00, 0xff), // 5 :Magenta
-        Color(0x00, 0xff, 0xff), // 6 :Cyan
-        Color(0xff, 0xff, 0xff), // 7 :White
+        Color(0xff, 0x00, 0xff), // 5 : Magenta
+        Color(0x00, 0xff, 0xff), // 6 : Cyan
+        Color(0xff, 0xff, 0xff), // 7 : White
         Color(0xc6, 0xc6, 0xc6), // 8 : Gray
         Color(0x84, 0x00, 0x00), // 9 : DrakRed
         Color(0x00, 0x84, 0x00), // 10: DarkGreen
@@ -99,11 +99,59 @@ impl Writer {
         self.buffer.pixels[y][x].write(code);
     }
 
-    pub fn write_all(&mut self, code: PaletteCode) {
-        for y in 0..VGA_HEIGHT {
-            for x in 0..VGA_WIDTH {
+    pub fn fill(&mut self, x0: usize, y0: usize, x1: usize, y1: usize, code: PaletteCode) {
+        for y in y0..=y1 {
+            for x in x0..=x1 {
                 self.write_pixel(x, y, code);
             }
         }
+    }
+
+    pub fn fill_all(&mut self, code: PaletteCode) {
+        self.fill(0, 0, VGA_WIDTH - 1, VGA_HEIGHT - 1, code);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{print, println};
+
+    #[test_case]
+    fn test_write_pixel() {
+        print!("test_write_pixel... ");
+        WRITER.lock().write_pixel(3, 5, PaletteCode::White);
+        println!("[ok]");
+    }
+
+    #[test_case]
+    fn test_fill_all() {
+        print!("test_fill_all... ");
+        WRITER.lock().fill_all(PaletteCode::White);
+        println!("[ok]");
+    }
+
+    #[test_case]
+    fn test_fill_output() {
+        print!("test_fill_output... ");
+
+        interrupts::without_interrupts(|| {
+            let mut writer = WRITER.lock();
+            writer.fill_all(PaletteCode::White);
+            writer.fill(5, 10, 15, 20, PaletteCode::DarkGray);
+            for x in 0..VGA_WIDTH {
+                for y in 0..VGA_HEIGHT {
+                    let expected_code = if 5 <= x && 10 <= y && x <= 15 && y <= 20 {
+                        PaletteCode::DarkGray
+                    } else {
+                        PaletteCode::White
+                    };
+                    let code = writer.buffer.pixels[y][x].read();
+                    assert_eq!(code, expected_code);
+                }
+            }
+        });
+
+        println!("[ok]");
     }
 }
